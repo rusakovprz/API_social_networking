29cd5b282c1915cadb2d2b081cf6f161a0d690be
Alexey <rusakovprz@rambler.ru>
2013-10-21 20:38:21 +0400

    Added function 'execute' in vk.py

--- vk.py
+++ vk.py
@@ -18,6 +18,10 @@ class vk():
     self.__configuration = configuration
     """" Имя узла предоставляющего API. """
     self.__host = 'api.vk.com'
+    self.__connect = httplib.HTTPSConnection(self.__host)
+  
+  def __del__(self):
+    pass
     
   def get_user(self, user_IDs):
     """
@@ -31,12 +35,14 @@ class vk():
       {"response":[{"uid":1,"first_name":"Павел","last_name":"Дуров"},{"uid":2,"first_name":"Александра","last_name":"Владимирова","hidden":1}]}
     """  
 
-    connect = httplib.HTTPSConnection(self.__host)
     url = "/method/users.get?user_ids=" + self.list_IDs_to_string(user_IDs) 
      
-    connect.request( "GET", url)	  
-    response=connect.getresponse()
+    self.__connect.request( "GET", url)	  
+    response=self.__connect.getresponse()
     print response.read()
+    
+    # TODO: print заменить на return, пи этом разобраться с кодировкой 
+
     
   @staticmethod  
   def list_IDs_to_string(user_IDs):
@@ -55,6 +61,44 @@ class vk():
       out_string += ','
     
     return out_string.rstrip(',')
+
+
+  def execute(self, method_name, *return_data, **in_arguments):
+    """
+      Универсальный метод, который позволяет запускать вызывать любой метод API,
+      с производьным числом аргументов и серриализацией возвращаемых данных.    
+    
+    >>> config = {}  
+    >>> obj = vk(config)
+    >>> user_IDs = vk.list_IDs_to_string( [1,2] )
+
+    >>> obj.execute("users.get", user_ids=user_IDs)
+    {"response":[{"uid":1,"first_name":"Павел","last_name":"Дуров"},{"uid":2,"first_name":"Александра","last_name":"Владимирова","hidden":1}]}
+    
+    """
+    #print method_name
+    #print return_data
+    #print in_arguments
+    
+    url = "/method/" + method_name
+    
+    if len(in_arguments) > 0:
+      url += "?"
+    
+    for key in in_arguments.keys():
+      url += key + "=" + in_arguments[key] + ","
+    
+    url.lstrip(",")
+    
+    #print "url = ", url
+    
+    self.__connect.request( "GET", url)	  
+    response=self.__connect.getresponse()
+    print response.read()
+    
+    # TODO
+    # - Серриализация возвращаемых данных на основании списка return_data
+    # - print заменить на return, пи этом разобраться с кодировкой        
     
     
 if __name__ == "__main__":
